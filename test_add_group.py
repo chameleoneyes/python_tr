8616bbbc606a002fa92d980b44515359f1aaabf9
chameleoneyes <khambikovar@gmail.com>
2021-10-05 14:06:21 +0300

    2 Задание. параметризация

--- test_add_group.py
+++ test_add_group.py
@@ -13,28 +13,28 @@ class TestAddGroup(unittest.TestCase):
     def test_add_group(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
-        self.login(wd)
-        self.create_group(wd)
+        self.login(wd, pwd="secret", login="admin")
+        self.create_group(wd, gname='test1', gheader='test2', gfooter='test2')
         wd.find_element_by_link_text("Logout").click()
 
-    def create_group(self, wd):
+    def create_group(self, wd, gname, gheader, gfooter):
         wd.find_element_by_link_text("groups").click()
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("test1")
+        wd.find_element_by_name("group_name").send_keys(gname)
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("test1")
+        wd.find_element_by_name("group_header").send_keys(gheader)
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("test1")
+        wd.find_element_by_name("group_footer").send_keys(gfooter)
         wd.find_element_by_name("submit").click()
         wd.find_element_by_link_text("group page").click()
 
-    def login(self, wd):
+    def login(self, wd, pwd, login):
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(login)
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(pwd)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def tearDown(self):
