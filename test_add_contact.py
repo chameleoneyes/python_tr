189c688c24039e10a706c33bca5877ea4b47a4d2
chameleoneyes <khambikovar@gmail.com>
2021-10-05 18:56:14 +0300

    3 Задание. add contact

---
+++ test_add_contact.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+import unittest
+from contact import Contact
+
+
+class TestAddContact(unittest.TestCase):
+    def setUp(self):
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
+
+    def test_add_contact(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/")
+        self.login(wd, pwd="secret", login="admin")
+        self.add_contact(wd, Contact(firstname="test1", lastname="test2", company="test3", mobile="89456464564"))
+
+
+
+    def login(self, wd, pwd, login):
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(login)
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(pwd)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+
+    def add_contact(self, wd, contact):
+        wd.find_element_by_link_text("add new").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile)
+        wd.find_element_by_name("submit").click()
+        wd.find_element_by_link_text("home page").click()
+
+    def tearDown(self):
+        self.wd.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
