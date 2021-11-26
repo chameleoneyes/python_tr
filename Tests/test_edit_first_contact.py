e766fef3868d83cc7f2612b7d38314b338a41fbf
chameleoneyes <khambikovar@gmail.com>
2021-11-26 12:16:14 +0300

    11 Задание. добавление сортировки списков на assert в тесте редактирования контакта

--- Tests/test_edit_first_contact.py
+++ Tests/test_edit_first_contact.py
@@ -10,6 +10,6 @@ def test_edit_first_contact(app):
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
     old_contacts[0] = contact
-    assert old_contacts == new_contacts
+    assert sorted(old_contacts) == sorted(new_contacts)
 
 
