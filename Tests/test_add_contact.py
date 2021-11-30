30cf28f1a706eeef56a192a2ef8fc3a1f94b8f95
chameleoneyes <khambikovar@gmail.com>
2021-11-30 16:12:17 +0300

    19 Задание. Загрузка ТД из файлов в формате json для контактов

--- Tests/test_add_contact.py
+++ Tests/test_add_contact.py
@@ -2,43 +2,10 @@
 
 from Model.contact import Contact
 import pytest
-import random
-import string
-import re
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
-    s = "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-    s = s.strip()
-    s = re.sub('\s+',' ', s)
-    return prefix + s
-
-
-def random_phone(maxlen):
-    symbols = string.digits + " +-()"
-    n = "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-    n = n.strip()
-    n = re.sub('\s+',' ', n)
-    return n
-
-
-def random_email(domain_len, bodylen):
-    symbols = string.ascii_letters + string.digits
-    body = "".join([random.choice(symbols) for i in range(random.randrange(bodylen))])
-    domain = "".join([random.choice(string.ascii_lowercase) for i in range(random.randrange(domain_len))])
-    return body + "@" + domain + "." + "".join([random.choice(string.ascii_lowercase) for i in range(random.randrange(2)+1)])
-
-
-tdata = [Contact(firstname=random_string("name", 10), lastname=random_string("lastname", 10), addr=random_string("", 20),
-                 homephone=random_phone(10), mobile=random_phone(12), workphone=random_phone(10), phone2=random_phone(15),
-                 email1=random_email(10, 5), email2=random_email(15, 8), email3=random_email(20, 15))
-         for i in range(5)
-         ]
-
-
-@pytest.mark.parametrize("contact", tdata, ids=[repr(x) for x in tdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
---
+++ data/contacts.py
@@ -0,0 +1,13 @@
+from Model.contact import Contact
+
+
+tdata = [Contact(firstname="name1", lastname="lastname1", addr="address1",
+                 homephone="82854654", mobile="2(5454)5464", workphone="+58456-4564", phone2="+55(544)-54564",
+                 email1="email11@email.me", email2="email12@email.me", email3="email13@email.me"),
+         Contact(firstname="name2", lastname="lastname2", addr="address2",
+                 homephone="282854654", mobile="2(5454)5464", workphone="+58456-4564", phone2="+55(544)-54564",
+                 email1="email21@email.me", email2="email22@email.me", email3="email23@email.me"),
+         Contact(firstname="name3", lastname="lastname3", addr="address3",
+                 homephone="82854654", mobile="2(5454)5464", workphone="+58456-4564", phone2="+55(544)-54564",
+                 email1="email31@email.me", email2="email32@email.me", email3="email33@email.me")
+         ]
